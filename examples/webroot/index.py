5e08f500070539e2cbb38550f58f6dd7d0f4cb13
Bjarni R. Einarsson <bre@klaki.net>
2020-11-19 10:28:22 +0000

    Flesh out the HTTPD example a bit

--- examples/webroot/index.py
+++ examples/webroot/index.py
@@ -1,3 +1,8 @@
+# A dynamically generated web page.
+#
+# Note that the entire HTTP/1.0 response is generated here, including the
+# HTTP/1.0 200 OK line and the HTTP headers.
+#
 try:
   import esp
   flash_size = '%d' % (esp.flash_size(),)
@@ -10,17 +15,27 @@ try:
 except:
   mhz = 'unknown'
 
-conn.reply(frame, (
-    'HTTP/1.0 200 OK\n'
-    'Content-Type: text/html\n'
-    '\n'
-    '<h1>Hello world!</h1>\n'
-    '<p>This is <b>%s</b> at %s, you are %s</p>\n'
-    '<p>I have a %s CPU and %s bytes of flash.</p>\n'
-    '<p>See also: <a href="/hello.html">hello</a><p>\n'
-    '<h3>Your request:</h3>\n'
-    '<pre>%s</pre>\n'
-  ) % (
+conn.reply(frame, ("""
+HTTP/1.0 200 OK
+Content-Type: text/html
+Cache-Control: max-age=300
+
+<html><head>
+  <link rel="icon" href="data:;base64,=">
+  <style>
+%s
+  </style>
+  <title>upagekite: A Pythonic Index</title>
+</head><body>
+  <h1>Hello world!</h1>
+  <p>This is <b>%s</b> at %s, you are %s</p>
+  <p>I have a %s CPU and %s bytes of flash.</p>
+  <p>See also: <a href="/hello.html">hello</a><p>
+  <h3>Your request:</h3>
+  <pre>%s</pre>
+</body></html>
+""") % (
+     open('/bootstrap/webroot/default.css').read(),  # Inline the CSS
      kite.name,
      time.time(),
      frame.remote_ip,
--- upagekite/httpd.py
+++ upagekite/httpd.py
@@ -14,9 +14,23 @@ import json
 
 
 class HTTPD:
+  MIMETYPES = {
+    'css': 'text/css',
+    'gif': 'image/gif',
+    'htm': 'text/html',
+    'html': 'text/html',
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'json': 'application/json',
+    'md': 'text/plain',
+    'pdf': 'application/pdf',
+    'txt': 'text/plain',
+    '_': 'application/octet-stream'}
+
   def __init__(self, name, webroot):
     self.name = name
     self.webroot = webroot
+    self.static_max_age = 3600
 
   def _err(self, code, msg, conn, frame):
       conn.reply(frame, (
@@ -24,6 +38,9 @@ class HTTPD:
           'Content-Type: text/plain\r\n'
           '\r\n%s\r\n'
         ) % (code, msg, msg))
+
+  def _mimetype(self, fn):
+    return self.MIMETYPES.get(fn.rsplit('.', 1)[-1], self.MIMETYPES['_'])
 
   def handle_http_request(self, kite, conn, frame):
     try:
@@ -61,12 +78,13 @@ class HTTPD:
           'httpd': self,
           'kite': kite, 'conn': conn, 'frame': frame})
       else:
-        mimetype = 'text/html'  # FIXME
+        mimetype = self._mimetype(filename)
         conn.reply(frame, (
             'HTTP/1.0 200 OK\r\n'
             'Content-Type: %s\r\n'
+            'Cache-control: max-age=%d\r\n'
             '\r\n'
-          ) % (mimetype,),
+          ) % (mimetype, self.static_max_age),
           eof=False)
         while True:
           data = fd.read(4096)
