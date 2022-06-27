59383d3c0d036a085e1af344f1b434027a038854
Bjarni R. Einarsson <bre@klaki.net>
2022-06-27 17:42:04 +0000

    Oh look, it is 2022

--- upagekite/websocket.py
+++ upagekite/websocket.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2021, The Beanstalks Project ehf. and Bjarni R. Einarsson.
+# Copyright (C) 2020-2022, The Beanstalks Project ehf. and Bjarni R. Einarsson.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
