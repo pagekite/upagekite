dd94819b55c6556a24bb7d6696c5d6df1f4d5c9c
Bjarni R. Einarsson <bre@klaki.net>
2022-07-14 16:34:06 +0000

    Call this v0.3.0 - Pico W support is a big deal

--- upagekite/proto.py
+++ upagekite/proto.py
@@ -28,7 +28,7 @@ from struct import unpack
 
 
 # This is also used by setup.py
-UPAGEKITE_VERSION = '0.2.4'
+UPAGEKITE_VERSION = '0.3.0'
 
 # This is a cache of DNS hints we have recived from the network.
 _DNS_HINTS = {}
